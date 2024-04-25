8e662528d2da432fb29b9aefcdfe0fce05ab22bb
GddgdgMen <antonsvirskiy690@gmail.com>
2024-04-25 15:29:19 +0300

    Added code

---
+++ src/test/java/AppIT.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppIT {
+
+    @Test
+    public void integrationTestShouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Integration test passed!");
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void unitTestShouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Unit test passed!");
+    }
+}
